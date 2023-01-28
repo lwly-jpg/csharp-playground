7ec760c61cdd48385393c6f6168945978d74f1d4
adon <adnlwly@gmail.com>
2023-01-28 17:57:43 +0000

    Write Calculator program

---
+++ CSharp.Intro/Calculator.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharp.Intro
+{
+  class Calculator
+  {
+    public void Calculate()
+    {
+      int result = 2 + 3;
+      Console.WriteLine("The result is {0:d}", result);
+    }
+  }
+}
--- CSharp.Intro/Program.cs
+++ CSharp.Intro/Program.cs
@@ -7,6 +7,7 @@ namespace CSharp.Intro
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      new Calculator().Calculate();
     }
   }
 }

1fe4a31d71a78a20d8b5187f3c3ab88c94d3858a
adon <adnlwly@gmail.com>
2023-01-28 18:08:42 +0000

    Write Clock class with DisplayTime method

--- CSharp.Intro/Program.cs
+++ CSharp.Intro/Program.cs
@@ -8,15 +8,24 @@ namespace CSharp.Intro
     {
       Console.WriteLine("Hello World!");
       new Calculator().Calculate();
+      new Clock().DisplayTime();
     }
   }
-
-    class Calculator
+  class Calculator
   {
     public void Calculate()
     {
       int result = 2 + 3;
       Console.WriteLine("The result is {0:d}", result);
+    }
+  }
+
+  class Clock {
+    public void DisplayTime()
+    {
+      DateTime dt = DateTime.Now;
+      String time = dt.ToString("HH:mm");
+      Console.WriteLine("The current time is {0:d}", time);
     }
   }
 }

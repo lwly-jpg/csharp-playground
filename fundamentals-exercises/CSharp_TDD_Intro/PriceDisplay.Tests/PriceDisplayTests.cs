da30f895699ddc62566e5935b7234824257e82cb
adon <adnlwly@gmail.com>
2023-01-29 16:01:43 +0000

    Write first test for GetPriceLabel()

--- fundamentals-exercises/CSharp_TDD_Intro/PriceDisplay.Tests/PriceDisplayTests.cs
+++ fundamentals-exercises/CSharp_TDD_Intro/PriceDisplay.Tests/PriceDisplayTests.cs
@@ -9,6 +9,16 @@ namespace PriceDisplay.Tests
       Assert.Pass();
     }
 
+    [Test]
+    [TestCase("kettle", 12.00)]
+
+    public void Print_ItemName_Price(string item, double price)
+    {
+        PriceDisplay priceDisplay = new PriceDisplay();
+        string actual = priceDisplay.GetPriceLabel(item, price);
+        string expected = "The price for item {0:d} is {1:0.00}";
+        Assert.That(actual, Is.EqualTo(expected));
+    }
 
   }
 }

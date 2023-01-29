34546eec54f13bef92a24368896538a4348bdb3b
adon <adnlwly@gmail.com>
2023-01-29 16:20:09 +0000

    Set up BrokenCalculator debugging exercise

---
+++ fundamentals-exercises/CSharp_TDD_Intro/BrokenCalculator/BrokenCalculator.cs
@@ -0,0 +1,10 @@
+// namespace BrokenCalculator
+// {
+//   public class BrokenCalculator
+//   {
+//     public String Add(int first, int second)
+//     {
+//       return first + second;
+//     }
+//   }
+// }

d6d32e1c8c7c875169e805cd437d3eafb2480828
adon <adnlwly@gmail.com>
2023-02-11 15:37:17 +0000

    Handle letter guessing in CLI

--- WordGame/Game/Program.cs
+++ WordGame/Game/Program.cs
@@ -9,8 +9,13 @@ namespace Game
       WordChooser chooser = new WordChooser();
       Game game = new Game(chooser);
       Console.WriteLine("Welcome! Today the word to guess is:");
-      Console.WriteLine(game.GetWordToGuess());
-
+      while (game.attempts > 0)
+      {
+        Console.WriteLine(game.GetWordToGuess());
+        Console.WriteLine($"Enter one letter to guess ({game.attempts} attemps remaining):");
+        char guess = Console.ReadKey(true).KeyChar;
+        game.CheckLetter(guess);
+      }
     }
   }
 }

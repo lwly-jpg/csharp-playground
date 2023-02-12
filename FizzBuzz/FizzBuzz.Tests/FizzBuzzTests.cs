896283c6390fc225b60bad9a19ae4a6554e569ea
adon <adnlwly@gmail.com>
2023-02-12 16:17:33 +0000

    Add first FizzBuzz test

--- FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -7,4 +7,18 @@ public class FizzBuzzTests
     {
         Assert.Pass();
     }
+
+    [Test]
+    [TestCase(1)]
+    [TestCase(6)]
+    [TestCase(19)]
+    [TestCase(37)]
+    [TestCase(202)]
+    public void For_NumbersNotDivisibleByThreeOrFive_ReturnsNumber(int x)
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int actual = fizzBuzz.For(x);
+        int expected = x;
+        Assert.That(expected, Is.EqualTo(actual));
+    }
 }
